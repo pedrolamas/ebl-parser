8d4af017a92b0cf3cfe271de8ce8bb8475454e86
Pedro Lamas <pedrolamas@gmail.com>
2020-02-14 10:09:41 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Ebl Parser
+
+**NOTE: THIS IS VERY EARLY WIP!**
+
+At the moment, this is mostly an investigation on how to parse sub-elements EBL data from a Zigbee OTA file parsed by [zigbee-herdsman-converters](https://github.com/Koenkk/zigbee-herdsman-converters)
+
+## Usage
+
+```
+node.exe index.js <path-to-ota-file>
+```
+
+## References
+
+- https://github.com/yqyunjie/Zigbee-Project/blob/2bb294718ac2652fc98c5de08fb4bbd417680e1a/firmware/EmberZNet/EM35x/hal/micro/cortexm3/bootloader/ebl.h
+- https://github.com/Koenkk/zigbee-herdsman-converters/blob/bda892b48a/ota/common.js
---
+++ index.js
@@ -0,0 +1,91 @@
+const upgradeFileIdentifier = Buffer.from([0x1e, 0xf1, 0xee, 0x0b]);
+const assert = require("assert");
+const fs = require("fs");
+const zhc = require("zigbee-herdsman-converters/ota/common");
+
+const imageSignature = 0xe350;
+
+const ebltagHeader = 0x0;
+const ebltagProg = 0xfe01;
+const ebltagMfgprog = 0x02fe;
+const ebltagEraseprog = 0xfd03;
+const ebltagEnd = 0xfc04;
+
+const filename = process.argv[2];
+
+const fileBuffer = fs.readFileSync(filename);
+
+const imageData = zhc.parseImage(fileBuffer);
+
+imageData.elements.forEach(element => {
+  const eblData = parseEbl(element.data);
+
+  console.log(eblData);
+});
+
+function parseEbl(buffer) {
+  const header = {
+    tag: buffer.readUInt16BE(0),
+    len: buffer.readUInt16BE(2),
+    version: buffer.readUInt16BE(4),
+    signature: buffer.readUInt16BE(6),
+    flashAddr: buffer.readUInt32BE(8),
+    aatCrc: buffer.readUInt32BE(12)
+  };
+
+  assert(
+    header.tag === ebltagHeader && header.signature === imageSignature,
+    "Not an EBL file"
+  );
+
+  const data = buffer.slice(16, 4 + header.len);
+
+  let position = 4 + header.len;
+  const elements = [];
+  while (position < buffer.length) {
+    const element = parseEblSubElement(buffer, position);
+
+    elements.push(element);
+
+    if (element.tag == ebltagEnd) {
+      break;
+    }
+
+    position += 4 + element.len;
+  }
+
+  return {
+    header,
+    data,
+    elements
+  };
+}
+
+function parseEblSubElement(data, position) {
+  const tag = data.readUInt16BE(position);
+  const len = data.readUInt16BE(position + 2);
+
+  switch (tag) {
+    case ebltagProg:
+    case ebltagMfgprog:
+    case ebltagEraseprog:
+      return {
+        tag,
+        len,
+        flashAddr: data.readUInt32BE(position + 4),
+        data: data.slice(position + 8, position + 4 + len)
+      };
+
+    case ebltagEnd:
+      return {
+        tag,
+        len,
+        eblCrc: data.readUInt32BE(position + 4)
+      };
+
+    default:
+      throw new Error(
+        "unknown tag 0x" + tag.toString(16) + " at position " + position
+      );
+  }
+}
